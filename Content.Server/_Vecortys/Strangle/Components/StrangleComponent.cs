3482cd56abf4263562321db887c79bc5320b150e
Rumnius <bystrygrom@gmail.com>
2025-07-18 09:39:51 +0300

    Deleted some changes to transfer to another branch

--- Content.Server/_Vecortys/Strangle/Components/StrangleComponent.cs
+++
@@ -1,8 +0,0 @@
-namespace Content.Server._Vecortys.Strangle.Components;
-
-[RegisterComponent]
-public sealed partial class StrangleComponent : Component
-{
-    [DataField]
-    public EntityUid Source;
-}
